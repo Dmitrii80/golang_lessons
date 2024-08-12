8883dbec17e7f14b2b9894fdea8d35e6ac4e6672
Армяновский Дмитрий Васильевич <armyanovskii@vtb.ru>
2024-08-13 00:15:06 +0500

    Изучаем Golang. Урок 4. Основы. Итератор for range и структура map

---
+++ src/Lesson_4/main.go
@@ -0,0 +1,91 @@
+// Изучаем Golang. Урок 4. Основы. Итератор for range и структура map
+
+package main
+
+import (
+	"fmt"
+
+	"github.com/mitchellh/mapstructure"
+)
+
+type Point struct { // Тип Структура Point
+	X int // Поля структуры
+	Y int // Поля структуры
+}
+
+func (p Point) method() { // Метод Структуры Point
+	fmt.Printf("call Point method p.X = %d\n", p.X)
+}
+
+func main() {
+	// Итератор for range
+	arr := []string{"a", "b", "c"}
+	for i, l := range arr {
+		fmt.Printf("arr[%d] = %s\n", i, l)
+	}
+
+	for _, l2 := range arr {
+		fmt.Printf("l2 = %s\n", l2)
+	}
+
+	for i2 := range arr {
+		fmt.Printf("i2 = %d\n", i2)
+	}
+
+	// Структура map
+	//pointsMap := map[string]Point{} // Объявили map и проинициализировали её как nil
+	pointsMap := map[string]Point{ // Объявили map и проинициализировали
+		"b": {
+			X: 13,
+			Y: 15,
+		},
+	}
+	//otherMap := map[string]int{} // Объявили map и проинициализировали её как nil
+	otherMap := map[string]int{ // Объявили map и проинициализировали
+		"x": 23,
+		"y": 25,
+	}
+	otherPointsMap := make(map[int]Point) // Объявили map и проинициализировали
+	var oneMorePointsMap map[string]Point // Объявили map, без инициализаций
+	pointsMap["a"] = Point{
+		X: 1,
+		Y: 2,
+	}
+	fmt.Println(pointsMap)
+	fmt.Println(pointsMap["a"])
+	fmt.Println(pointsMap["a"].X)
+	fmt.Println(pointsMap["a"].Y)
+	pointsMap["a"].method()
+
+	fmt.Println(otherMap)
+	fmt.Println(otherMap["x"], otherMap["y"])
+	p1 := Point{}
+	mapstructure.Decode(otherMap, &p1) // Декодируем в структуру Point
+	fmt.Println("Декодируем в структуру Point:", p1)
+	fmt.Println(p1.X, p1.Y)
+	for k, v := range otherMap { // Итерация Map
+		fmt.Printf("key=%s value=%d\n", k, v)
+	}
+
+	otherPointsMap[1] = Point{X: 1, Y: 2}
+	fmt.Println(otherPointsMap[1])
+
+	if oneMorePointsMap == nil {
+		//oneMorePointsMap = map[string]Point{} // Проинициализировали её как nil
+		oneMorePointsMap = map[string]Point{
+			"a": {Y: 1, X: 2},
+		}
+	}
+	fmt.Println(oneMorePointsMap)
+
+	// Проверяем наличия ключа в Map
+	key := "a"
+	value, ok := oneMorePointsMap[key]
+	if ok {
+		fmt.Printf("key=%s exist in map\n", key)
+		fmt.Println(value)
+	} else {
+		fmt.Printf("key=%s does exist in map\n", key)
+		fmt.Println(value)
+	}
+}
